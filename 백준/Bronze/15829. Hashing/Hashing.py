a45e096b7c8e3a2af77113fa77a4266a8319c2c5
oyatplum <108467989+oyatplum@users.noreply.github.com>
2024-08-09 14:59:37 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -7,10 +7,11 @@ L = int(input())
 stringList = list(input().strip())
 
 sum = 0
+M = 1234567891
 
 for i in range(L):
     for j in alphaList:
         if stringList[i] == j:
-            sum = sum + (alphaList.index(j) + 1)*(31**i)
+            sum = sum + ((alphaList.index(j) + 1)*(31**i))
 
-print(sum)
+print(sum % M)
