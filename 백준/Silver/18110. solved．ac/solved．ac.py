ecc5e1f08d372e98bce1147afaa4afbe227399b5
oyatplum <108467989+oyatplum@users.noreply.github.com>
2024-09-27 09:05:01 +0900

    [Silver IV] Title: solved.ac, Time: 180 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -1,12 +1,8 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
-average = 0
-total = 0
 inputList = []
-queue = deque()
 
 def roundFunc(num):
     if num - int(num) >= 0.5:
@@ -14,25 +10,13 @@ def roundFunc(num):
     else:
         return int(num)
 
-percent = roundFunc(N*0.15)
-
 if N == 0:
     print(0)
 else:
     for _ in range(N):
         inputList.append(int(input()))
-
+    percent = roundFunc(N*0.15)
     inputList.sort()
-
-    for i in inputList:
-        queue.append(i)
-
-    for _ in range(percent):
-        queue.popleft()
-        queue.pop()
-
-    for i in queue:
-        total += i
-
-    average = roundFunc(total / len(queue))
+    inputList = inputList[percent:N-percent]
+    average = roundFunc(sum(inputList) / len(inputList))
     print(average)
