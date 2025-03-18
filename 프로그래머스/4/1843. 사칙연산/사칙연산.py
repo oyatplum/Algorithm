8eafc1e3600b72e025f8b5887c77608947aa23e9
oyatplum <oyatpeach@naver.com>
2025-03-18 16:55:18 +0900

    [level 4] Title: 사칙연산, Time: 110.61 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -0,0 +1,34 @@
+M, m = {}, {}
+def solution(arr):
+    answer = -1
+    nums = [int(x) for x in arr[::2]] #피연산자 배열
+    ops = [x for x in arr[1::2]] #연산자 배열
+    
+    for i in range(len(nums)):
+        M[(i, i)] = nums[i]
+        m[(i, i)] = nums[i]
+    
+    for d in range(1, len(nums)):
+        for i in range(len(nums)):
+            j = i + d
+            if j >= len(nums):
+                continue
+                
+            maxcandidates, mincandidates = [], []
+            
+            for k in range(i+1, j+1):
+                if ops[k-1] == '-':
+                    mx = M[(i, k-1)] - m[(k,j)]
+                    mn = m[(i, k-1)] - M[(k,j)]
+                    maxcandidates.append(mx)
+                    mincandidates.append(mn)
+                else:
+                    mx = M[(i, k-1)] + M[(k,j)]
+                    mn = m[(i, k-1)] + m[(k,j)]
+                    maxcandidates.append(mx)
+                    mincandidates.append(mn)
+                    
+            M[(i, j)] = max(maxcandidates)
+            m[(i, j)] = min(mincandidates)
+            
+    return M[(0, len(nums) - 1)]
