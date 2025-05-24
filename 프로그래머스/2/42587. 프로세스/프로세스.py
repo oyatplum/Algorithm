ce73f4c588eecf71c13bd84092bd5a4a2f4ca6a0
oyatplum <oyatpeach@naver.com>
2025-05-24 11:33:23 +0900

    [level 2] Title: 프로세스, Time: 0.30 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,18 +1,29 @@
+from collections import deque
+
 def solution(priorities, location):
     answer = 0
-    while priorities:
-        maxN = max(priorities)
-        if priorities[0] < maxN: #다시 뒤로
-            temp = priorities.pop(0)
-            priorities.append(temp)
-            if location == 0:
-                location = len(priorities) - 1
+    temp = deque()
+    for idx, p in enumerate(priorities): #location 비교 위해
+        temp.append([idx, p])
+    
+    while True:
+        maxNum = 0
+        for target in temp: # 가장 큰 수 구해놓기
+            if maxNum < target[1]:
+                maxNum = target[1]
+        
+        copied = temp.copy()
+        
+        for target in copied:
+            if target[1] < maxNum:
+                poped = temp.popleft()
+                temp.append(poped)
             else:
-                location -= 1
-        else: #pop
-            priorities.pop(0)
-            answer += 1
-            if location == 0:
-                return answer
-            else:
-                location -= 1
+                poped = temp.popleft()
+                answer += 1
+                if poped[0] == location:
+                    return answer
+                else:
+                    break         
+    
+    return answer
