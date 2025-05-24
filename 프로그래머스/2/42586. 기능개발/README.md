1a04a52b37eb866c09e23c9ad4fbdbdc8b68e1c2
oyatplum <oyatpeach@naver.com>
2025-05-24 11:17:00 +0900

    [level 2] Title: 기능개발, Time: 0.53 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.56 ms
+메모리: 9.29 MB, 시간: 0.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 06일 20:04:37
+2025년 05월 24일 11:16:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,28 @@
-answer = []
-
+from collections import deque
+def check(pro, sp):
+    while pro[0] < 100:
+        for idx, (p,s) in enumerate(zip(pro, sp)):
+            pro[idx] = p + s
+    return pro
+    
 def solution(progresses, speeds):
-    global answer
-    num = 0
-    while progresses[0] < 100:
-        for i, (j,k) in enumerate(zip(progresses, speeds)):
-            progresses[i] = j + k
-
-    for i in progresses[:]:
-        if i >= 100:
-            progresses.pop(0)
-            speeds.pop(0)
-            num += 1
-        else:
-            break
-    answer.append(num)
-
-    if len(progresses) == 0:
-        return answer
-    else:
-        return solution(progresses, speeds)
+    answer = []
+    pro = deque(progresses)
+    sp = deque(speeds)
+    
+    while pro:
+        returned = check(pro, sp)
+        co = returned.copy()
+        temp = 0
+        
+        for i in co:
+            if i >= 100:
+                pro.popleft()
+                sp.popleft()
+                temp += 1
+            else:
+                break
+        if temp > 0:
+            answer.append(temp)
+    
+    return answer
