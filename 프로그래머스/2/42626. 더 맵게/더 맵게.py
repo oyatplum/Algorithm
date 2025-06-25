0c05d980b64c60f5caa85a861e9734f1cdddd3f0
oyatplum <oyatpeach@naver.com>
2025-06-25 20:33:33 +0900

    [level 2] Title: 더 맵게, Time: 1728.86 ms, Memory: 48.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -2,13 +2,14 @@ import heapq
 def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
-
+    
     while scoville[0] < K:
         if len(scoville) == 1:
             return -1
         first = heapq.heappop(scoville)
         second = heapq.heappop(scoville)
-        new = first + second*2
+        new = first + second * 2
         heapq.heappush(scoville, new)
         answer += 1
+    
     return answer
