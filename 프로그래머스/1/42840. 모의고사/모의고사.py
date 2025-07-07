c946009d53693e53bb0b02816fddc76e50f1e870
oyatplum <oyatpeach@naver.com>
2025-07-07 19:26:54 +0900

    [level 1] Title: 모의고사, Time: 2.28 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,21 @@
 def solution(answers):
     answer = []
-    first = [1,2,3,4,5] #5
-    second = [2,1,2,3,2,4,2,5] #8
-    third = [3,3,1,1,2,2,4,4,5,5] #10
-    temp = [0,0,0]
+    first = [1, 2, 3, 4, 5] #5개
+    second = [2, 1, 2, 3, 2, 4, 2, 5] #8개
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] #10개
+    temp = [0, 0, 0]
     
-    for idx, a in enumerate(answers):
-        if a == first[idx%5]:
+    for idx, i in enumerate(answers):
+        if i == first[idx % 5]:
             temp[0] += 1
-        if a == second[idx%8]:
+        if i == second[idx % 8]:
             temp[1] += 1
-        if a == third[idx%10]:
+        if i == third[idx % 10]:
             temp[2] += 1
-            
+    
     maxNum = max(temp)
     
     for idx, t in enumerate(temp):
-        if t == maxNum:
+        if maxNum == t:
             answer.append(idx+1)
-
     return answer
