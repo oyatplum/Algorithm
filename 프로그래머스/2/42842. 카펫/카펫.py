50fc89c9744e8063f698dbe7af669f2018ea6bb7
oyatplum <oyatpeach@naver.com>
2025-07-09 23:03:48 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,18 +1,19 @@
 def solution(brown, yellow):
     answer = []
     total = brown + yellow
-    temp = []
-    
+
     for i in range(1, total+1):
         if total % i == 0:
-            temp.append(i)
-    
-    for row in temp:
-        col = total // row
+            w = i
+            h = total // i
+            
+            if (w-2) * (h-2) == yellow:
+                if w >= h:
+                    answer.append(w)
+                    answer.append(h)
+                else:
+                    answer.append(h)
+                    answer.append(w)
+                break
         
-        if (col - 2) * (row - 2) == yellow:
-            answer.append(row)
-            answer.append(col)
-            break
-    answer.sort(reverse = True)
     return answer
