5126882d4525961803ba2af5ceadf05b31c628cd
oyatplum <oyatpeach@naver.com>
2025-07-10 11:22:16 +0900

    [level 2] Title: 피로도, Time: 53.71 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,16 @@
 from itertools import permutations
-
 def solution(k, dungeons):
-    answer = 0
-    temp = list(permutations(dungeons, len(dungeons)))
+    answer = -1
+    permutes = list(permutations(dungeons, len(dungeons)))
     
-    for dungeon in temp:
-        posK = k
+    for perm in permutes:
+        cur = k
         total = 0
-        for d in dungeon:
-            if posK >= d[0]: #던전 참여 가능
-                posK -= d[1]
+        for p in perm:
+            if cur >= p[0] and cur - p[1] >= 0:
+                cur -= p[1]
                 total += 1
-        if answer < total:
-            answer = total
-    
+        if total > answer:
+            answer = total 
+                
     return answer
