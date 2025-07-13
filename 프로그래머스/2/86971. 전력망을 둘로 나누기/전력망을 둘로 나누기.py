a713e5f159083dd3f99790c72d77a3a8a5342130
oyatplum <oyatpeach@naver.com>
2025-07-13 15:08:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.29 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,35 +1,39 @@
-from collections import defaultdict
-def dfs(start, graph, visited):
-    visited[start] = 1
-    count = 1
-    for nextNode in graph[start]:
-        if visited[nextNode] != 1:
-            count += dfs(nextNode, graph, visited)
-    return count
-    
 def solution(n, wires):
-    answer = -1
-    arr = []
-    for i in range(len(wires)):
-        copyWires = wires.copy()
-        del copyWires[i]
-        graph = defaultdict(list)
+    temp = []
+    
+    graph = [[] for _ in range(n+1)]
+    
+    for wire in wires:
+        graph[wire[0]].append(wire[1])
+        graph[wire[1]].append(wire[0])
+    
+    def dfs(start, visited, count):
+        count = 1
+        
+        for node in graph[start]:
+            if visited[node] == 0:
+                visited[node] = 1
+                count += dfs(node, visited, count)
+        return count
+        
+    for wire in wires:
+        visited1 = [0] * (n+1)
+        visited2 = [0] * (n+1)
+        
+        visited1[wire[0]] = 1
+        visited2[wire[1]] = 1
+        
+        graph[wire[0]].remove(wire[1])
+        graph[wire[1]].remove(wire[0])
+        
+        count = 0
+        
+        first = dfs(wire[0], visited1, count)
+        second = dfs(wire[1], visited2, count)
+        
+        temp.append(abs(first - second))
+        
+        graph[wire[0]].append(wire[1])
+        graph[wire[1]].append(wire[0])
 
-        for a,b in copyWires:
-            graph[a].append(b)
-            graph[b].append(a)
-        visited = [0] * (n+1)
-        
-        temp = []
-        for i in range(1, n+1):
-            returned = dfs(i, graph, visited)
-            if returned == 1:
-                continue
-            else:
-                temp.append(returned)
-        
-        if len(temp) == 1:
-            arr.append(temp[0]-1)
-        else:
-            arr.append(abs(temp[0] - temp[1]))
-    return min(arr)
+    return min(temp)
