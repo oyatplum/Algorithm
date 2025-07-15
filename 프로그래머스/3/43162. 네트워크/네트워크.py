05bbc4c26f3757f397fcaec1dbcab7fafffd2831
oyatplum <oyatpeach@naver.com>
2025-07-15 14:28:39 +0900

    [level 3] Title: 네트워크, Time: 0.64 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,21 @@
-def dfs(start, visited, computers):
-    if visited[start] == 0:
-        visited[start] = 1
-        for i in range(len(computers)):   
-            if visited[i] == 0 and computers[start][i] == 1 and i != start:
-                dfs(i, visited, computers)
-        return 1
-    else:
-        return 0
-    
-            
 def solution(n, computers):
     answer = 0
     visited = [0] * n
     
+    def dfs(targetIdx):
+        nonlocal visited
+        
+        for idx, computer in enumerate(computers[targetIdx]):
+            if idx != targetIdx and visited[idx] != 1 and computer == 1:
+                visited[idx] = 1
+                dfs(idx)
+    
     for i in range(n):
-        returned = dfs(i, visited, computers)
-        answer += returned
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(i)
+            answer += 1
+        else:
+            continue
+    
     return answer
