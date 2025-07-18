95acea9cd0d8493c08ab86b52c0acd97adda52f6
oyatplum <oyatpeach@naver.com>
2025-07-18 11:00:36 +0900

    [level 3] Title: 단어 변환, Time: 0.34 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,31 +1,27 @@
 from collections import deque
 def solution(begin, target, words):
-    answer = 0
-    q = deque()
-    visited = [begin]
-    
-    if target not in words: #변환할 수 없는 경우    
+    if target not in words:
         return 0
     
+    answer = 0
+    q = deque()
     q.append(begin)
+    visited = [begin]
     
     while q:
-        answer += 1
         for _ in range(len(q)):
             poped = q.popleft()
             for word in words:
-                if word not in visited: #중복 제거위해
-                    same = 0
-                    diff = 0
-                    for p, w in zip(poped, word):
-                        if p == w:
-                            same += 1
-                        else:
+                diff = 0
+                if word not in visited:
+                    for i, j in zip(poped, word):
+                        if i != j:
                             diff += 1
-                    if same == len(poped) - 1 and diff == 1:
+                    if diff == 1:
                         if word == target:
-                            return answer
-                        visited.append(word)
-                        q.append(word)
-    
+                            return answer + 1
+                        else:
+                            visited.append(word)
+                            q.append(word)
+        answer += 1
     return answer
