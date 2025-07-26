874fc4d470d246e27aa483a267bd64be5ca32b96
oyatplum <oyatpeach@naver.com>
2025-07-26 17:34:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 55.80 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,22 +1,23 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    ingTruck = deque()
-    totalWeight = 0
-    readyTrucks = deque(truck_weights)
+    time = 0
+    q = deque(truck_weights)
+    totalWeight = 0 #다리 위 트럭 총 무게
+    ing = deque([0] * bridge_length) #건너는 트럭
     
-    while readyTrucks:
-        poped = readyTrucks.popleft()
-        if len(ingTruck) == bridge_length:
-            iPoped = ingTruck.popleft()
-            totalWeight -= iPoped
-        if totalWeight + poped <= weight and len(ingTruck) < bridge_length:
-            ingTruck.append(poped)
-            totalWeight += poped
-            answer += 1
+    while totalWeight > 0 or q: #처음 트럭이 올라가기 전에 시작해야해서, q에 트럭 모두 사라지고 ing에 트럭 모두 건널 때까지
+        time += 1
+        popedIng = ing.popleft() #건너는 트럭 한칸씩 앞으로
+        totalWeight -= popedIng
+        
+        if q:
+            if totalWeight + q[0] <= weight: #다음 트럭 올라갈 수 있으면
+                popedTruck = q.popleft()
+                ing.append(popedTruck)
+                totalWeight += popedTruck
+            else:
+                ing.append(0)
         else:
-            ingTruck.append(0)
-            readyTrucks.appendleft(poped)
-            answer += 1
-
-    return answer + bridge_length
+            ing.append(0)
+    
+    return time
