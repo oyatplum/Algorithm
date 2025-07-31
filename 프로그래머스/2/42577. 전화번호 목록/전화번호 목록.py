1ef9f687ff8e8a0be59f8126d03329d8873b32d8
oyatplum <oyatpeach@naver.com>
2025-07-31 10:39:21 +0900

    [level 2] Title: 전화번호 목록, Time: 90.56 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,6 @@
 def solution(phone_book):
     phone_book.sort()
-    
-    for p1, p2 in zip(phone_book, phone_book[1:]):
-        if p2.startswith(p1):
+    for i, j in zip(phone_book, phone_book[1:]):
+        if j.startswith(i):
             return False
-        
     return True
